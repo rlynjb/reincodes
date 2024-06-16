3b19c61b91dc0ee1095d0a0d3b4689d43fdfec73
System Administrator <root@Reinas-MacBook-Air.local>
2024-06-16 14:56:05 -0700

    add todo notes for implementing CRUD

--- apps/cheatsheets/src/app/features/ViewNote.tsx
+++ apps/cheatsheets/src/app/features/ViewNote.tsx
@@ -7,6 +7,18 @@ interface Props {
   desc?: string
 }
 
+/**
+ * TODO:
+ * 1. setup QuillJS and render desc values
+ * 2. move sample values to desc property
+ * 3. convert Note Title to input field
+ * 4. convert How To column values to input field
+ * 5. convert Side Nav Books and Chapters to input field
+ * 6. add "Add Note" form
+ * 7. add "Add Book" form
+ * 8. add "Add Chapter" form
+*/
+
 export const ViewNote: FC<Props> = ({title, sample = ``, desc = ''}) => {
   return (
     <div className="collapse bg-base-200 rounded-none">
